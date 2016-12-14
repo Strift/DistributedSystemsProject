6e3eeff0189a0fe2b9d5729bd08f08b6049fd50b
Laurent <lau.cazanove@gmail.com>
2016-12-14 16:39:42 +0100

    Clean and generic code for Server instanciation

--- src/project/server/Server.java
+++ src/project/server/Server.java
@@ -1,6 +1,9 @@
 package project.server;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -10,37 +13,46 @@ import project.remote.Sorter;
 public class Server {
 
 	private static final String SERVICE_NAME = "Sorter";
+	
+	private static int instances = 0;
 
 	public static void main(String[] args) throws Exception {
+		Server server1 = new Server();
+		Server server2 = new Server();
+		try {
+			server1.start();
+			server2.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String name;
+	
+	public Server() {
+		Server.instances++;
+		this.name = "Server" + Server.instances;
+	}
+	
+	public void start() throws UnknownHostException, RemoteException, NotBoundException {
 		// check the name of the local machine (two methods)
-		System.out.println("Server: running on host " + InetAddress.getLocalHost());
-		System.out.println("Server: hostname property " + System.getProperty("java.rmi.server.hostname"));
+		System.out.println(this.name + ": running on host " + InetAddress.getLocalHost());
+		System.out.println(this.name + ": hostname property " + System.getProperty("java.rmi.server.hostname"));
 		
 		// instanciate the remote object
 		Sorter sorter = new SimpleSorter();
-		System.out.println("Server: instanciated SimpleSorter");
+		System.out.println(this.name + ": instanciated SimpleSorter");
 		
 		// create a skeleton and a stub for that remote object
 		Sorter stub = (Sorter) UnicastRemoteObject.exportObject(sorter, 0);
-		System.out.println("Server: generated skeleton and stub");
+		System.out.println(this.name + ": generated skeleton and stub");
 		
 		// register the remote object's stub in the registry
 		Registry registry = LocateGlobalRegistry.getRegistry();
-		registry.rebind(SERVICE_NAME, stub);
-		System.out.println("Server: registered remote object's stub");
-		
-		// Print list
-		//Server.printRegistryList(registry.list());
+		registry.rebind(this.name + "." + SERVICE_NAME, stub);
+		System.out.println(this.name + ": registered remote object's stub");
 		
 		// main terminates here, but the JVM still runs because of the skeleton
-		System.out.println("Server: ready");
-	}
-	
-	public static void printRegistryList(String[] list) {
-		System.out.println("New registry list contains: ");
-		for (final String str : list) {
-			System.out.print(str + ", ");
-		}
-		System.out.print("\n");
+		System.out.println(this.name + ": ready");
 	}
 }
